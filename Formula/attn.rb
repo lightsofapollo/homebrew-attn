1e96ce6262ff2269691d0767369761516e43aea5
James Lal <james@littlebearlabs.io>
2026-03-03 23:42:43 +0000

    Update attn formula to 0.3.4

--- Formula/attn.rb
+++ Formula/attn.rb
@@ -2,19 +2,19 @@ class Attn < Formula
   desc "A beautiful markdown viewer that launches from the CLI"
   homepage "https://github.com/lightsofapollo/attn"
   license "MIT"
-  version "0.3.3"
+  version "0.3.4"
 
   on_macos do
     on_arm do
       url "https://github.com/lightsofapollo/attn/releases/download/v#{version}/attn-v#{version}-darwin-arm64.app.zip"
-      sha256 "536665cf8bbeebaebfe9245ad86dcd6c1fb37e7500dbcf28c8b7f0b6c76ac38f"
+      sha256 "dcf1c11606679cb58b37e8344c39a322af36cb3f1fd140b1ee0111a38c523472"
     end
   end
 
   on_linux do
     on_intel do
       url "https://github.com/lightsofapollo/attn/releases/download/v#{version}/attn-v#{version}-linux-x64"
-      sha256 "97e27c953a2f223c46d349f5befa04b4ca3598e87f9ddaaf87142f4a1630e242"
+      sha256 "9a5d50e04b5909c036597dfe3053c01b9ba879853a61bcb8cbfc0771eb06fce0"
     end
   end
 
