0e36a9cd4a114fd18fc4102d99404cd73dbc035e
James Lal <james@littlebearlabs.io>
2026-03-03 23:11:22 +0000

    Update attn formula to 0.3.2

--- Formula/attn.rb
+++ Formula/attn.rb
@@ -2,29 +2,34 @@ class Attn < Formula
   desc "A beautiful markdown viewer that launches from the CLI"
   homepage "https://github.com/lightsofapollo/attn"
   license "MIT"
-  version "0.3.1"
+  version "0.3.2"
 
   on_macos do
     on_arm do
-      url "https://github.com/lightsofapollo/attn/releases/download/v#{version}/attn-v#{version}-darwin-arm64"
-      sha256 "a2d6a376b2b743cb5c7203cc08ca36017ac85b0a41d0beacd25193fad17fb8c3"
+      url "https://github.com/lightsofapollo/attn/releases/download/v#{version}/attn-v#{version}-darwin-arm64.app.zip"
+      sha256 "a74daec88962afd5de22b8a3539aa4b6a54215dcf1ce909e556456fac17717cc"
     end
   end
 
   on_linux do
     on_intel do
       url "https://github.com/lightsofapollo/attn/releases/download/v#{version}/attn-v#{version}-linux-x64"
-      sha256 "287778d816dfca6b3eb0265bbdf4bc8ad58003edce3a501fc6865ed9bd719b09"
+      sha256 "142c68e4eeafb5eeeb3d1e127e1173e154809bbc3de5dc7719ce2c46cd6c8845"
     end
   end
 
   def install
-    downloaded = Dir["attn-v*"].first
-    if downloaded
-      mv downloaded, "attn"
+    if OS.mac?
+      prefix.install "attn.app"
+      bin.install_symlink prefix/"attn.app/Contents/MacOS/attn"
+    else
+      downloaded = Dir["attn-v*"].first
+      if downloaded
+        mv downloaded, "attn"
+      end
+      chmod 0755, "attn"
+      bin.install "attn"
     end
-    chmod 0755, "attn"
-    bin.install "attn"
   end
 
   test do
