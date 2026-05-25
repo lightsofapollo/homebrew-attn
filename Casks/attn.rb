e97601bcda024470de5c78394e38178a2f283f42
James Lal <james@littlebearlabs.io>
2026-05-25 00:33:08 +0000

    Update attn cask to 0.6.7

--- Casks/attn.rb
+++ Casks/attn.rb
@@ -1,6 +1,6 @@
 cask "attn" do
-  version "0.6.6"
-  sha256 "a892d7e2624612f034b2b327ec0497f6f7d69e964448bf148770806d9e0a1134"
+  version "0.6.7"
+  sha256 "c92610ce4c174e827d5ea63fc32a6d475a1f0270736e136b47fcce24d4d3574c"
 
   url "https://github.com/lightsofapollo/attn/releases/download/v#{version}/attn-v#{version}-darwin-arm64.app.zip"
   name "attn"
